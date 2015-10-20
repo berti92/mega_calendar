203e9d268a94a385a6bf01996a1e3f39d91e5b50
berti <berti@PC-Berti>
2015-10-20 20:59:43 +0200

    Calendar: Added tooltip with further informations Calendar: Closed issues are better visible

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,52 @@ class CalendarController < ApplicationController
   def index
     #DO NOTHING
   end
+
+  def form_holiday(holiday)
+    ret_var = '<table>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'user') + '</td>'
+    ret_var << '<td>' + holiday.user.login + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'start') + '</td>'
+    ret_var << '<td>' + holiday.start.to_date.to_s + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'end') + '</td>'
+    ret_var << '<td>' + holiday.end.to_date.to_s + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '</table>'
+    return ret_var
+  end
+  def form_issue(issue)
+    ticket_time = TicketTime.where({:issue_id => issue.id}).first rescue nil
+    tbegin = ticket_time.time_begin.strftime(" %H:%M") rescue ''
+    tend = ticket_time.time_end.strftime(" %H:%M") rescue ''
+    ret_var = '<table>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'project') + '</td>'
+    ret_var << '<td>' + issue.project.name + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'status') + '</td>'
+    ret_var << '<td>' + issue.status.name + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'user') + '</td>'
+    ret_var << '<td>' + issue.assigned_to.login + '</td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'start') + '</td>'
+    ret_var << '<td>' + issue.start_date.to_date.to_s + ' ' + tbegin + ' </td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '<tr>'
+    ret_var << '<td>' + (translate 'end') + '</td>'
+    ret_var << '<td>' + issue.due_date.to_date.to_s + ' ' + tend + ' </td>' rescue '<td></td>'
+    ret_var << '</tr>'
+    ret_var << '</table>'
+    return ret_var
+  end
   def get_events
     fbegin = params[:start].to_date rescue nil
     fend = params[:end].to_date rescue nil
@@ -15,13 +61,17 @@ class CalendarController < ApplicationController
     @events = []
     def_holiday = '#' + Setting.plugin_mega_calendar['default_holiday_color']
     def_color = '#' + Setting.plugin_mega_calendar['default_event_color']
-    @events = @events + holidays.collect {|h| {:title => h.user.login + ' - ' + (translate 'holiday'), :start => h.start.to_date.to_s, :end => h.end.to_date.to_s, :allDay => true, :color => def_holiday, :url => '/holidays/show?id=' + h.id.to_s, :className => 'calendar_event' }}
+    @events = @events + holidays.collect {|h| {:title => h.user.login + ' - ' + (translate 'holiday'), :start => h.start.to_date.to_s, :end => h.end.to_date.to_s, :allDay => true, :color => def_holiday, :url => '/holidays/show?id=' + h.id.to_s, :className => 'calendar_event', :description => form_holiday(h) }}
     issues.each do |i|
       ticket_time = TicketTime.where({:issue_id => i.id}).first rescue nil
       tbegin = ticket_time.time_begin.strftime(" %H:%M") rescue ''
       tend = ticket_time.time_end.strftime(" %H:%M") rescue ''
+      css_classes = ['calendar_event']
+      if !i.status.blank? && i.status.is_closed == true
+        css_classes << 'calendar_event_closed'
+      end
       color = '#' + UserColor.where({:user_id => i.assigned_to_id}).first.color_code rescue def_color
-      @events << {:title => i.id.to_s + ' - ' + i.subject, :start => i.start_date.to_date.to_s + tbegin, :end => i.due_date.to_date.to_s + tend, :color => color, :url => '/issues/' + i.id.to_s, :className => 'calendar_event' }
+      @events << {:title => i.id.to_s + ' - ' + i.subject, :start => i.start_date.to_date.to_s + tbegin, :end => i.due_date.to_date.to_s + tend, :color => color, :url => '/issues/' + i.id.to_s, :className => css_classes, :description => form_issue(i) }
     end
     render(:text => @events.to_json.html_safe)
   end
