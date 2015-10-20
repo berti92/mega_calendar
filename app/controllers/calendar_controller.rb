1530de2d087bff14474c60ce55330c3b8fa6bade
berti <berti@PC-Berti>
2015-10-20 20:14:55 +0200

    Calendar: Added onclick event Issues/Show: Bugfix

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -15,13 +15,13 @@ class CalendarController < ApplicationController
     @events = []
     def_holiday = '#' + Setting.plugin_mega_calendar['default_holiday_color']
     def_color = '#' + Setting.plugin_mega_calendar['default_event_color']
-    @events = @events + holidays.collect {|h| {:title => h.user.login + ' - ' + (translate 'holiday'), :start => h.start.to_date.to_s, :end => h.end.to_date.to_s, :allDay => true, :color => def_holiday }}
+    @events = @events + holidays.collect {|h| {:title => h.user.login + ' - ' + (translate 'holiday'), :start => h.start.to_date.to_s, :end => h.end.to_date.to_s, :allDay => true, :color => def_holiday, :url => '/holidays/show?id=' + h.id.to_s, :className => 'calendar_event' }}
     issues.each do |i|
       ticket_time = TicketTime.where({:issue_id => i.id}).first rescue nil
       tbegin = ticket_time.time_begin.strftime(" %H:%M") rescue ''
       tend = ticket_time.time_end.strftime(" %H:%M") rescue ''
       color = '#' + UserColor.where({:user_id => i.assigned_to_id}).first.color_code rescue def_color
-      @events << {:title => i.id.to_s + ' - ' + i.subject, :start => i.start_date.to_date.to_s + tbegin, :end => i.due_date.to_date.to_s + tend, :color => color }
+      @events << {:title => i.id.to_s + ' - ' + i.subject, :start => i.start_date.to_date.to_s + tbegin, :end => i.due_date.to_date.to_s + tend, :color => color, :url => '/issues/' + i.id.to_s, :className => 'calendar_event' }
     end
     render(:text => @events.to_json.html_safe)
   end
